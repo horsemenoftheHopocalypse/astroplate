664adc5eec6351fe6c59985ea3fc3d15cf67cbc0
Barry Forrest <barry.forrest@gmail.com>
2025-12-30 14:51:31 -0600

    add a `no jekyll` file

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -4,10 +4,7 @@ import starlight from '@astrojs/starlight';
 import starlightThemeBlack from 'starlight-theme-black'
 
 export default defineConfig({
-  //base: '/docs/',
-  site: 'https://horsemenoftheHopocalypse.github.io/astroplate/',
-  // if the docs site is served at the repo root on Pages, omit base
-  // base: '/<something>'   // only needed if you serve from a sub-path
+  site: 'https://horsemenofthehops.github.io',
   integrations: [
     starlight({
       plugins: [
