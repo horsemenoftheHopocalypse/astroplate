6b45a0e754b6af7f894c8d508ed7fd799a2d2d46
Barry Forrest <barry.forrest@gmail.com>
2025-12-30 14:28:25 -0600

    documentation underway

--- docs/docs/astro.config.mjs
+++
@@ -1,8 +0,0 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
-
-export default defineConfig({
-  site: 'https://horsemenoftheHopocalypse.github.io/astroplate/',
-  // if the docs site is served at the repo root on Pages, omit base
-  // base: '/<something>'   // only needed if you serve from a sub-path
-});
