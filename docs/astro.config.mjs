217bcff39ef83e7df9a3c1613b02d09fff3410cc
Barry Forrest <barry.forrest@gmail.com>
2026-03-26 12:40:33 -0500

    update dependencies

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -8,13 +8,23 @@ export default defineConfig({
   base: '/docs/',
   integrations: [
     starlight({
+      title: "Horsemen of the Hops Site Documentation",
+      sidebar: [
+        {
+          label: 'Guides',
+          autogenerate: {directory: 'guides'}
+        },
+        {
+          label: 'Reference',
+          autogenerate: {directory: 'reference'},
+        }
+      ],
       plugins: [
         starlightThemeBlack({
           footerText: //optional
             '© 2026 Horsemen of the Hopocalypse. All rights reserved.',
         })
       ],
-      title: 'Horsemen of the Hops Docs',
     }),
   ],
 });
